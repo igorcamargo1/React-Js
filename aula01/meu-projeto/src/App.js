b68dd087a05500b5c51e2000c639dc7573f27882
igorcamargo <rm94553@fiap.com.br>
2022-08-25 20:30:25 -0300

    entendendo o jsx

--- aula01/meu-projeto/src/App.js
+++ aula01/meu-projeto/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 
 function App() {
+  
+  const name = 'Igor'
+  
+  const newName = name.toUpperCase()
+  
+  function sum(a, b){
+    return a + b
+  }
+
+  const url= "https://via.placeholder.com/150"
+
   return (
-    <div className="App">
-      <h1>Olá React</h1>
+    <div/* aqui é usado o className, e não somente o class */ className="App">
+      <h1>Olá React, meu nome é {newName}</h1>
       <p>Meu primeiro App</p>
+      <p>soma: {sum(1,2)}</p>
+      <img src={url} alt ="minha imagem"/>
     </div>
   )
 }
