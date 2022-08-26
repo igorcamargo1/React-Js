8ae379ff1b488479d8dae7f0bbacb0c9379fb7b2
igorcamargo <rm94553@fiap.com.br>
2022-08-25 21:02:31 -0300

    Aprendendo um pouco mais sobre components

--- aula01/meu-projeto/src/App.js
+++ aula01/meu-projeto/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import HelloWorld from './components/helloWord/HelloWorld';
 
 function App() {
   
@@ -15,6 +16,7 @@ function App() {
   return (
     <div/* aqui é usado o className, e não somente o class */ className="App">
       <h1>Olá React, meu nome é {newName}</h1>
+      < HelloWorld/>
       <p>Meu primeiro App</p>
       <p>soma: {sum(1,2)}</p>
       <img src={url} alt ="minha imagem"/>
---
+++ aula01/meu-projeto/src/components/frase/Frase.js
@@ -0,0 +1,9 @@
+function Frase(){
+    return(
+        <div>
+            <p>Impontando um componente dentro do outro</p>
+        </div>
+    )
+}
+
+export default Frase
