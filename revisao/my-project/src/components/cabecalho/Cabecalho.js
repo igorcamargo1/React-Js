6fd9599cb0ec2cc5c53c49d0da37c243155fd7d1
igorcamargo <rm94553@fiap.com.br>
2022-09-01 23:18:45 -0300

    treinando react

---
+++ revisao/my-project/src/components/cabecalho/Cabecalho.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Cabecalho(){
+    
+    return(
+        <header>
+            <h1>Jobable</h1>
+            <p>Aqui você é diferente.</p>
+        </header>
+    )
+}
---
+++ revisao/my-project/src/components/carros/Carros.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Carros(){
+    
+    return(
+        <img src="" alt="" />
+    )
+}
