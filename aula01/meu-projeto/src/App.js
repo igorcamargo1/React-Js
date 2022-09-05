268fdfe789860b0941716ce6d28e31b7d7ea05b2
igorcamargo <rm94553@fiap.com.br>
2022-09-05 20:51:33 -0300

    implementando o react router

--- aula01/meu-projeto/src/App.js
+++ aula01/meu-projeto/src/App.js
@@ -1,33 +1,22 @@
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Home from './pages/Home'
 import Empresa from './pages/Empresa'
 import Contato from './pages/Contato'
+import Navbar from './components/layout/navbar/Navbar'
+import Footer from './components/layout/footer/Footer'
 
 function App() {
   
 
   return (
     <Router>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-      </ul>
-      <ul>
-        <li><Link to='/empresa'>Home</Link></li>
-      </ul>
-      <ul>
-        <li><Link to='/contato'>Home</Link></li>
-      </ul>
-      <Switch>
-        <Route exact path="/">
-          <Home/>
-        </Route>
-        <Route path="/empresa">
-          <Empresa/>
-        </Route>
-        <Route path="/contato">
-          <Contato/>
-        </Route>
-      </Switch>
+      <Navbar/>
+      <Routes>
+        <Route exact/*muito importante*/ path='/' element={<Home/>}></Route>
+        <Route path='/empresa' element={<Empresa/>}></Route>
+        <Route path='/contato' element={<Contato/>}></Route>
+      </Routes>
+      <Footer/>
     </Router>
   )
 }
