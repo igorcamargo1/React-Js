8c3e34bcb2e85c95bd179e353d80c0f1b6a12226
igorcamargo <rm94553@fiap.com.br>
2022-09-05 21:16:08 -0300

    importando icones

--- aula01/meu-projeto/src/components/layout/footer/Footer.js
+++ aula01/meu-projeto/src/components/layout/footer/Footer.js
@@ -1,3 +1,14 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 export default function Footer(){
-    return <footer>Rodapé</footer>
+    return <footer>
+                <p>Rodapé</p>
+                <ul className={styles.social_list}>
+                    <li><FaFacebook/></li>
+                    <li><FaInstagram/></li>
+                    <li><FaLinkedin/></li>
+                </ul>
+                <p>Rodapé</p>
+            </footer>
 }
