4c5f7c56a6a521e49cb1387d93f833c57efad12c
igorcamargo <rm94553@fiap.com.br>
2022-09-05 23:00:53 -0300

    estruturando o projeto

--- projeto/costs/src/App.js
+++ projeto/costs/src/App.js
@@ -1,8 +1,34 @@
 import './index.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/container/Container';
+
+
 
 function App() {
   return (
-    <p>Costs</p>
+    <Router>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/contact'>Contact</Link>
+        <Link to='/company'>Company</Link>
+        <Link to='/newProject'>New Project</Link>
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route path='/company' element={<Company />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/newProject' element={<NewProject />}></Route>
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
   )
 }
 
