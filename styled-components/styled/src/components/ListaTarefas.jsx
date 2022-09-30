b0c7602a13ada95b903611fbd19cba14aa5e6621
igorcamargo <rm94553@fiap.com.br>
2022-09-30 19:53:14 -0300

    addTarefas criado

--- styled-components/styled/src/components/ListaTarefas.jsx
+++ styled-components/styled/src/components/ListaTarefas.jsx
@@ -1,10 +1,69 @@
 import React from 'react'
+import Tarefa from './Tarefa'
+import styled from 'styled-components'
+import { useState } from 'react'
+
+const DivLista = styled.div`
+    width: 100%;
+    min-height: 85vh;
+    background-color: #ffb;
+    padding: 20px;
+    border: 2px solid #ccc;
+    display: flex; flex-wrap: wrap;
+    justify-content: space-around;
+`
 
 export default function ListaTarefas() {
+    
+    const [tarefa, setTarefa] = useState([
+        {
+            titulo:"Lista de Pagamento",
+            setor:"Dep.vendas",
+            descricao:"Levantar os valores de vendas"
+        },
+        {
+            titulo:"Planilha de salarios",
+            setor:"Dep. Pessoal",
+            descricao:"Gerar planilhas"
+        },
+        {
+            titulo:"Lanças notas do challenge",
+            setor:"Graduação",
+            descricao:"Lançar notas dos grupos no portal"
+        }
+    ])
+    
+    //CRIANDO UMA FUNÇÃO PARA ADICIONAR TAREFAS
+    const addTarefa = ()=>{
+        
+        const novaTarefa={
+            titulo:"Planilha de notas",
+            setor:"Graduação",
+            descricao:"Lançar quem não fez exercicios"
+        }
+
+        setTarefa([...tarefa, novaTarefa])
+    }
+
+
     return (
         <div>
             <p>lista de tarefas</p>
-            
+            <div>
+                <button onClick={addTarefa}>Add-tarefa</button>
+            </div>
+            <DivLista>
+                {tarefa.map((t,i)=>
+                <Tarefa
+                    key={i}
+                    id={i+1}
+                    titulo={t.titulo}
+                    setor={t.setor}
+                    descricao={t.descricao}
+                />
+                )}
+            </DivLista>
+            <p>lista de tarefas</p>
         </div>
     )
 }
--- styled-components/styled/src/components/Tarefa.jsx
+++ styled-components/styled/src/components/Tarefa.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+// CRIANDO UM MÓDULO
+const DivTarefa = styled.div`
+    background-color:#ffb;
+    border: 2px solid #333;
+    box-shadow: 5px 5px 5px #333;
+    padding:2vh 2vw ;
+    text-align:center;
+    width: 50vw;
+    height: 20vh;
+    margin: 5vh 30vw;
+    h2,p{ padding-bottom:10px; }
+`
 
-export default function Tarefa() {
+
+export default function Tarefa(props) {
     return (
-        <div>Tarefa</div>
+        <DivTarefa>
+            <h2>{props.titulo}</h2>
+            <p>{props.setor}</p>
+            <p>{props.descricao}</p>
+            <p>{props.id}</p>
+        </DivTarefa>
     )
 }
