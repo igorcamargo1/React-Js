f37dd4ffd51409f4937a5077be2475c5484793ac
igorcamargo <rm94553@fiap.com.br>
2022-09-30 18:54:50 -0300

    index.css + ListaTarefa.jsx

--- styled-components/styled/src/App.jsx
+++ styled-components/styled/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ListaTarefas from './components/ListaTarefas'
 
 export default function App() {
   return (
     <div>
       <h1>App</h1>
+      <ListaTarefas/>
     </div>
   )
 }
---
+++ styled-components/styled/src/components/ListaTarefas.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ListaTarefas() {
+    return (
+        <div>
+            <p>lista de tarefas</p>
+            
+        </div>
+    )
+}
