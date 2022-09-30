6ab63dbb4ac8133a2eb82576b88f91012cefa47c
igorcamargo <rm94553@fiap.com.br>
2022-09-30 19:59:46 -0300

    formtarefa adicionado

---
+++ styled-components/styled/src/components/FormTarefas.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function FormTarefas() {
+    return (
+        <div>
+            
+            <form method="post">
+
+                <fieldset>
+                    <legend>Tarefas</legend>
+                    <div>
+                        <label>Título</label>
+                        <input type="text" name='titulo' placeholder='Titulo...' value="" />
+                    </div>
+                    <div>
+                        <label>Setor</label>
+                        <input type="text" name='setor' placeholder='setor...' value="" />
+                    </div>
+                    <div>
+                        <label>Descrição</label>
+                        <textarea name='descricao' cols="30" rows="10" placeholder='Descrição...' value=""></textarea>
+                    </div>
+                    <div>
+                        <input type="submit" value='Adicionar' />
+                    </div>
+                </fieldset>
+
+            </form>
+
+        </div>
+    )
+}
