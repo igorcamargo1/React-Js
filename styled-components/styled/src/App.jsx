336093605bd8dda8c6e0313c6091cf40e7435b50
igorcamargo <rm94553@fiap.com.br>
2022-09-30 20:19:48 -0300

    finalizando rotas

--- styled-components/styled/src/App.jsx
+++ styled-components/styled/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Home from './components/Home'
 import ListaTarefas from './components/ListaTarefas'
+import MainRoutes from './routes/MainRoutes'
 
 export default function App() {
   return (
     <div>
       <h1>App</h1>
-      <ListaTarefas/>
+      <Home></Home>
+      <MainRoutes/>
     </div>
   )
 }
--- styled-components/styled/src/components/Home.jsx
+++ styled-components/styled/src/components/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
         <div>
             <header>
                 <nav>
-                    
+                    <Link to="/">Home</Link>
+                    <Link to="/lt">Lista de tarefas</Link>
+                    <Link to="/">Adicionar tarefas</Link>
                 </nav>
             </header>
         </div>
